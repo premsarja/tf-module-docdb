f7b48ec06611252ca26312c895e554c72948d5fb
premsarja <premsagar@prodot.in>
2024-03-13 17:21:24 +0530

    done

--- sg.tf
+++ sg.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "allows_docdb" {
     from_port   = 27017
     to_port     = 27017
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR]
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_CIDR[0]]
   }
 
   egress {
