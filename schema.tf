2273187c70417bcbfc16491e5536c280352814f6
premsarja <premsagar@prodot.in>
2024-03-20 11:05:49 +0530

    done

--- schema.tf
+++ schema.tf
@@ -4,22 +4,15 @@ resource "null_resource" "schema" {
   provisioner "local-exec" {
     command = <<EOF
       cd /tmp
-      # Download certificate authority file
       wget https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-      
-      # Download MongoDB archive and extract
       curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
       unzip -o /tmp/mongodb.zip
-      
-      # Navigate to MongoDB directory
       cd mongodb-main
-      
-      # List files in directory (for debugging)
       ls -ltr   
-      
-      # Execute MongoDB commands
       mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username prem --password premsagar < catalogue.js
       mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint} --sslCAFile global-bundle.pem --username prem --password premsagar < users.js 
+ 
     EOF
   }
 }
+
