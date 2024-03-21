c72718d943afdcad064a6b012e78e001de8ea039
premsarja <premsagar@prodot.in>
2024-03-21 15:32:17 +0530

    done

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 300
-  records = [aws_eip.lb.public_ip]
-}
+resource "aws_route53_record" "docdb_dns" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "mondodb-${var.ENV}-${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_docdb_cluster.docdb.endpoint]
+}   
