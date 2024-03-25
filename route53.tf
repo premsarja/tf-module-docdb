f47cbfb2b45ec5568f5d70734d5b6c793d4e0e11
premsarja <premsagar@prodot.in>
2024-03-25 13:59:13 +0530

    done

--- route53.tf
+++ route53.tf
@@ -1,4 +1,4 @@
- resource "aws_route53_record" "docdb_dns" {
+ resource "aws_route53_record" "www" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
   name    = "mongodb-${var.ENV}-${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "CNAME"
