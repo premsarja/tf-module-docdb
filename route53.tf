9c2c5f67bc10cff4c0fcf163274b9d916d781989
premsarja <premsagar@prodot.in>
2024-03-25 14:10:45 +0530

    done

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
  resource "aws_route53_record" "docdb" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "mongodb-${var.ENV}-${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  name    = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
   records = [aws_docdb_cluster.docdb.endpoint]
